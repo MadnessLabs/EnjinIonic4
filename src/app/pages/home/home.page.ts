4ec51acec9acfaeeb7ab1665d8908d6966c711e1
Popcorn245 <Bobby@madnesslabs.net>
2018-05-28 14:21:32 -0500

    first commit

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NFC, Ndef } from '@ionic-native/nfc';
 
 @Component({
   selector: 'app-page-home',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nfc: NFC;
+  ndef: Ndef;
 
+  constructor() {
+    const theWindow:any = window;
+    this.nfc = theWindow.nfc;
+    this.ndef = theWindow.ndef;
+    if (this.nfc) {
+      this.nfc.addNdefListener((nfcEvent: any) => {
+        console.log(this.nfc.bytesToHexString(nfcEvent.tag.id));
+      });
+    }
+  }
+
+  public writeTag() {
+    if (!this.ndef) {
+      alert('This device does not support NFC!');
+      return false;
+    }
+    const message = [
+      this.ndef.textRecord("hello, world", null, null),
+      this.ndef.uriRecord("http://github.com/chariotsolutions/phonegap-nfc", null)
+    ];
+    this.nfc.write(message).then(data => {
+      console.log(data);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 }
